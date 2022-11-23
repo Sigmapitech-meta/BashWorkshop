5808070365d434b1af9ffdb059dc4ae1615770fc
Sigmanificient <edhyjox@gmail.com>
2022-11-23 15:54:20 +0100

    :sparkles: Add solution for 6: letter_count

---
+++ src/6_letter_count.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sum=0
+
+if [ -z "$1" ]
+then
+  echo "Error: missing argument"
+  exit 1
+fi
+
+for file in *;
+do
+  count=$(cat $file | grep -o "$1" | wc -l)
+  sum=$((sum + count))
+  echo "$count fois in : $file"
+done
+echo "Soit $sum fois dans le dossier"
