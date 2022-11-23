6b432ac12cbc68e094fc8f3e2a3ef54ebd815cd7
Sigmanificient <edhyjox@gmail.com>
2022-11-23 15:55:02 +0100

    :sparkles: Add solution for 10: weather

---
+++ src/10_weather.bash
@@ -0,0 +1,8 @@
+echo -ne "Retrieving meteo...\r"
+
+ip=$(curl -s https://api.ipify.org)
+loc=$(curl -s https://ipapi.co/$ip/city)
+
+meteo=$(curl -s https://wttr.in/$loc)
+
+echo "$meteo" | head -n -3
