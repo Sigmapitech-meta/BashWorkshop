180c85a6cb45c05a60d3e1ad3920dab256bb1c29
Sigmanificient <edhyjox@gmail.com>
2022-11-23 15:54:12 +0100

    :sparkles: Add solution for 5: auto_commit

---
+++ src/5_auto_commit.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+read -p "Type de commits: " -r type
+read -p "Fichier concernées: " -r file
+read -p "Message: " -r message
+
+file_f=${file// /, }
+
+echo "git add $file"
+echo -e "git commit -m \"$type ($file_f): $message\" &&\n git push"
+
+read -p "Executer ? (y/n)" -r exec
+
+if [ "$exec" = "y" ]; then
+    git add "$file"
+    git commit -m "$type ($file_f): $message" && git push
+    echo "Success"
+else
+    echo "Aborted"
+fi
