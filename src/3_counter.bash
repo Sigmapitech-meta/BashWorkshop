9e126be1f2268fc43f99113d94cfb508ba70a5c7
Sigmanificient <edhyjox@gmail.com>
2022-11-23 15:53:50 +0100

    :sparkles: Add solution for 3: counter

---
+++ src/3_counter.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+arg=$1
+if [ -z "$1" ]
+then
+  echo "Error: missing argument"
+  exit 1
+fi
+
+if [[ $arg =~ ^[0-9]+$ ]];
+then
+  for ((i=1; i<=arg; i++));
+  do
+    echo "$i"
+  done
+else
+  echo "Error argument shouldbe a number"
+fi
